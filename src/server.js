906e01261209c3557a6c7392cdfd3c1ee630ad8d
Pete Isgrigg <sobchak7@gmail.com>
2017-02-13 22:10:05 -0500

    Updated file with notes and a starting route

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 //Require modules and files needed by assigning to variables
 const express = require('express');
-const config = require(."/config");
+const config = require("./config");
+const router = require('./routes');
 
 //set APP variable  as express in order to use set up apps
 const app = express();
 
+//display the data from routes/index.js
+app.use(router);
 
+//Setes up server to allow client to connect at port 8080
 app.listen(config.port, ()=>{
   console.log("THE FRONTEND SERVER IS LISTENING ON PORT " + config.port + "!!!!");
 })
