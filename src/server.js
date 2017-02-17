ff24d5bc318b81fb825e372f715b787b846848e5
Pete Isgrigg <sobchak7@gmail.com>
2017-02-17 18:01:20 -0500

    Added public folders for for js,font,css and added a static redirect for css. Also added bottstrap and jquery as dependecies. Added a simple css file to style template.

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const config = require("./config");
 const router = require('./routes');
 const path = require('path');
 
+
 //set APP variable  as express in order to use set up apps
 const app = express();
 
@@ -11,8 +12,11 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
+//set up express static server for css,js, and oher public facing files for frontend.
+app.use(express.static(path.join(__dirname,'/public')));
+
 //display the data from routes/index.js
-app.use(router);
+app.use(router)
 
 //Setes up server to allow client to connect at port 8080
 app.listen(config.port, ()=>{
