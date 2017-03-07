e3b1174dde1ffb938704ebe414e1be325e2ace13
Pete Isgrigg <sobchak7@gmail.com>
2017-03-07 16:55:09 -0500

    Updated static paths

--- src/server.js
+++ src/server.js
@@ -2,19 +2,22 @@
 const express = require('express');
 const config = require("./config");
 const router = require('./routes');
+const bodyParser = require('body-parser')
 const path = require('path');
 
 
 //set APP variable  as express in order to use set up apps
 const app = express();
 
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 //setting up Pug as template
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 //set up express static server for css,js, and oher public facing files for frontend. Also set up static for node_modules
-app.use(express.static(path.join(__dirname,'/public')));
-app.use('/node_modules',express.static(path.join(__dirname,'/node_modules')));
+app.use(express.static(path.join(__dirname,'../public')));
+app.use('/node_modules',express.static(path.join(__dirname,'../node_modules')));
 
 
 //display the data from routes/index.js
